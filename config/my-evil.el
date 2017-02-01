66b516f063ef81ac2109731968064486bb7302af
askobeldin <askobeldin@gmail.com>
2017-02-01 13:42:25 +0300

    modified 01 feb

--- config/my-evil.el
+++ config/my-evil.el
@@ -2,9 +2,6 @@
 ;;
 ;; Settings for ostracizing me from both the Emacs and Vim communities.
 ;; a.k.a. evil-mode
-;;
-;; last change: 02 авг 2015 16:13:28
-
 
 ;; In order to work properly, we need to load evil-leader-mode before we load
 ;; evil-mode.
@@ -13,10 +10,9 @@
   :ensure evil-leader
   :demand evil-leader
   :config
-	  (progn
-		(evil-leader/set-leader ",")
-		(global-evil-leader-mode t)))
-
+    (progn
+        (evil-leader/set-leader ",")
+        (global-evil-leader-mode t)))
 
 ;; Here's what we've all been waiting for.
 ;; Recreate Vim inside Emacs.
@@ -28,18 +24,15 @@
     (evil-mode 1)
     (setq evil-want-C-u-scroll t)
     (setq evil-want-C-w-in-emacs-state t)
-    (setq evil-search-module        'isearch)
-    (setq evil-magic                'very-magic)
-
+    (setq evil-search-module  'isearch)
+    (setq evil-magic          'very-magic)
 
     (setq evil-want-fine-undo t)
     (setq evil-want-change-word-to-end t)
 
-
     (use-package evil-nerd-commenter
       :ensure evil-nerd-commenter
       :commands (evilnc-comment-or-uncomment-lines))
-
 
     (use-package evil-matchit
       :ensure evil-matchit
@@ -48,7 +41,6 @@
       (progn
         (setq global-evil-matchit-mode t)
         (define-key evil-normal-state-map "%" 'evilmi-jump-items)))
-
 
     (use-package evil-surround
       :ensure evil-surround
@@ -64,15 +56,15 @@
       (progn
         (key-chord-mode 1)))
 
-
     (define-key evil-normal-state-map (kbd "C-w }") 'evil-window-rotate-downwards)
     (define-key evil-normal-state-map (kbd "C-w {") 'evil-window-rotate-upwards)
-
 
     (define-key evil-normal-state-map (kbd "C-h")   'evil-window-left)
     (define-key evil-normal-state-map (kbd "C-j")   'evil-window-down)
     (define-key evil-normal-state-map (kbd "C-k")   'evil-window-up)
     (define-key evil-normal-state-map (kbd "C-l")   'evil-window-right)
+
+    (define-key evil-normal-state-map (kbd "C-<tab>") 'evil-next-buffer)
 
     (define-key evil-normal-state-map "a"           'evil-append)
 
@@ -85,13 +77,9 @@
 
     (define-key evil-normal-state-map "/"           'evil-search-forward)
 
-
     (evil-ex-define-cmd "Q"  'evil-quit)
     (evil-ex-define-cmd "Qa" 'evil-quit-all)
-    (evil-ex-define-cmd "QA" 'evil-quit-all)
-
-    ))
-
+    (evil-ex-define-cmd "QA" 'evil-quit-all)))
 
 (require 'evil-visualstar)
 (global-evil-visualstar-mode)
